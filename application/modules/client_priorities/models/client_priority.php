c2e04d0c7e11f5940b0b12b080072892159dffa3
pujie <pw.prayitno@gmail.com>
2017-09-05 17:02:31 +0700

    add files

--- application/modules/client_priorities/models/client_priority.php
+++ application/modules/client_priorities/models/client_priority.php
@@ -1,31 +1,41 @@
 <?php
-class Client_priority extends DataMapper{
+class Client_priority extends CI_Model{
+	var $ci;
 	function __construct(){
 		parent::__construct();
+		$this->ci = & get_instance();
 	}
-	
 	function client_exist($client_id){
-		$obj = new Client_priority();
-		$obj->where('client_id',$client_id)->get();
-		if($obj->result_count()>0){
+		$sql = "select count(id) total from client_priorities ";
+		$sql.= "where client_id='".$client_id."'";
+		$que = $this->ci->db->query($sql);
+		$res = $que->result();
+		if($res[0]->num_rows()>0){
 			return true;
 		}else{
 			return false;
 		}
 	}
-	
 	function add($params){
-		$obj = new Client_priority();
-		foreach($params as $key=>$val){
-			$obj->$key = $val;
+		$keys = array();
+		$vals = array();
+		$sql = "";
+		foreach($params as $key => $val ){
+			array_push($keys,$key);
+			array_push($vals,$val);
 		}
-		$obj->save();
-		return $this->db->insert_id();
-	}
-	
+		$keystring = "('" . implode("','",$keys) . "')";
+		$valstring = "('" . implode("','",$vals) . "')";
+		$sql = "insert into client_priorities ";
+		$sql.= " " . $keystring . " " ;
+		$sql.= " values " ;
+		$sql.= " " . $valstring . " " ;
+		$que = $this->ci->db->query($sql);
+		return $this->ci->db->insert_id();
+	}	
 	function drop($params){
-		$obj = new Client_priority();
-		$obj->where('client_id',$params['client_id'])->get();
-		$obj->delete();
+		$sql = "delete from client_priorities ";
+		$sql.= "where client_id='".$params["client_id"]."'";
+		$que = $this->ci->db->query($sql);
 	}
 }
--- application/modules/datacenters/models/datacenter.php
+++ application/modules/datacenters/models/datacenter.php
@@ -1,22 +1,28 @@
 <?php
-class Datacenter extends Datamapper{
+class Datacenter extends CI_Model{
+	var $ci;
 	function __construct(){
 		parent::__construct();
+		$this->ci = & get_instance();
 	}
 	function getbyid($id){
-		$obj = new Datacenter();
-		$obj->where('id',$id)->get();
-		return $obj;
+		$sql = "select * from datacenters ";
+		$sql.= "where id='".$id."'";
+		$que = $this->ci->db->query($sql);
+		return $que->result()[0];
 	}	
 	function get_combo_data($first_data=''){
 		$out = array();
 		if($first_data!=''){
 			$out[0] = $first_data;
 		}
-		$objs = new Datacenter();
-		$objs->where('active','1')->get();
-		foreach ($objs as $obj){
-			$out[$obj->id] = $obj->name;
+		$sql = "select a.id,b.name,b.address from datacenters a ";
+		$sql.= "where active='1' ";
+		$sql.= "order by name asc ";
+		$que = $this->ci->db->query($sql);
+		$out = array();
+		foreach ($que->result() as $res){
+			$out[$res->id] = $res->name;
 		}
 		return $out;
 	}
